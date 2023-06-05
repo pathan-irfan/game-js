7d2c28ea36cca507c3d84d1388887e19c29d3bd7
irfan pathan <you@example.com>
2023-06-05 17:28:01 +0530

    Game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const container = document.querySelector("#container");
 const dino = document.querySelector("#dino");
 const block = document.querySelector("#block");
@@ -17,7 +18,7 @@ window.addEventListener("keydown", (e) => {
   if (e.key === " ") {
     gameover.style.display = "none";
     block.classList.add("blockActive");
-    road.firstElementChild.style.animation = "roadAnimation 1.5s linear infinite";
+    road.firstElementChild.style.animation = "roadAnimation 3s linear infinite";
     playerScore = 0;
     interval = setInterval(scoreCounter, 200);
   } else if (e.key === "ArrowUp") {
@@ -38,6 +39,8 @@ setInterval(() => {
 
   if (dinoBottom <= 50 && blockLeft >= 20 && blockLeft <= 105) {
 
+    
+
     gameover.style.display = "block";
     block.classList.remove("blockActive");
     road.firstElementChild.style.animation = "none";
@@ -46,4 +49,38 @@ setInterval(() => {
 
    
   }
+  if(playerScore == 100){
+  
+    road.firstElementChild.style.animation = "roadAnimation 2.6s linear infinite";
+    container.style.backgroundImage="url(/images/backround1.webp)"
+    
+  }
+  if(playerScore == 300){
+  
+    road.firstElementChild.style.animation = "roadAnimation 1.6s linear infinite";
+    container.style.backgroundImage="url(/images/background2.webp)"
+    
+  }
+
+ 
+    
 }, 10);
+
+
+
+
+// if(playerScore == 500){
+  
+//   road.firstElementChild.style.animation = "roadAnimation 2.6s linear infinite";
+//   container.style.backgroundImage="url(/GN.avif)"
+// }
+// if(playerScore == 150){
+//   road.firstElementChild.style.animation = "roadAnimation 2s linear infinite";
+  
+// }
+
+// if(playerScore == 900){
+  
+//   road.firstElementChild.style.animation = "roadAnimation 0.1s linear infinite";
+
+// }
